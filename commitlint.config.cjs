4fcd1619d33ac1bdc9dbea80acb4dde7cc253865
Alireza Mohammadzadeh (NedaMani) <alireza3205@gmail.com>
2026-06-14 09:52:02 +0330

    feat: add commitlint package

---
+++ commitlint.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+	extends: ["@commitlint/config-conventional"],
+	rules: {
+		"type-enum": [2, "always", ["feat", "fix", "chore", "docs", "refactor"]],
+		"subject-case": [2, "never", ["start-case", "pascal-case"]],
+		"scope-case": [0],
+	},
+};
