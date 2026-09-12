5ad3f1442afdf24bf9905a68836a86a8ef8f0b62
Alireza Mohammadzadeh (NedaMani) <alireza3205@gmail.com>
2026-09-12 18:32:21 +0330

    chore: update packages & add tsdown

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "tsup";
+import { defineConfig } from "tsdown";
 
 export default defineConfig({
   entry: ["src/index.ts", "src/gregorian.ts", "src/hijri.ts", "src/jalali.ts"],
+
   format: ["cjs", "esm"],
+
   dts: true,
-  splitting: false,
+
   sourcemap: true,
+
   clean: true,
 });
